e0ec584c2671139251b701ca113735cc1bd1d98e
Legalbox <contact@legalbox.com>
2016-04-19 11:32:26 +0200

    Update APIResourcesManager.php

--- API/APIResourcesManager.php
+++ API/APIResourcesManager.php
@@ -26,9 +26,9 @@ namespace API;
 
 class APIResourcesManager
 {
-	public static $urlApplication = 'https://mail.legalbox.com/restful/application';
-	public static $urlSession = 'https://mail.legalbox.com/restful/session';
-	public static $uploadUrl = 'https://mail.legalbox.com/restful/fileUpload';
-	public static $downloadUrl = 'https://mail.legalbox.com/restful/fileDownload';
+	public static $urlApplication = 'https://certified.legalbox.com/restful/application';
+	public static $urlSession = 'https://certified.legalbox.com/restful/session';
+	public static $uploadUrl = 'https://certified.legalbox.com/restful/fileUpload';
+	public static $downloadUrl = 'https://certified.legalbox.com/restful/fileDownload';
 		
 }
